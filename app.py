cb6ff6e03ce2668566f0bde8dff3df74130800b0
FreeZe06 <tim.vannson@ynov.com>
2024-11-13 16:26:19 +0100

    errorhandler 404 405 500

--- app.py
+++ app.py
@@ -172,3 +172,16 @@ if __name__ == "__main__":
         db.create_all()
         populate_database(10)
     app.run(port=8000)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return jsonify({'error': 'Internal Server Error', 'message': str(error)}), 500
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({'error': 'Not Found', 'message': str(error)}), 404
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({'error': 'Method Not Allowed', 'message': str(error)}), 405
